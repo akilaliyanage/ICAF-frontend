b145c58d655bc5f6c986ed61f5cbad61aff2637f
Akila Liyanage <akila.liyanage@madmobile.com>
2021-05-26 10:20:24 +0530

    landing page 0%

--- components/landing_page/About.jsx
+++ components/landing_page/About.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import config from '../../config.json'
 
 class About  extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            des : ''
+         }
     }
+
+    componentDidMount(){
+        fetch(config.host + "/about").then(res => res.json()).then(data =>{
+            console.log(data[0]["des"])
+            this.setState({des : data[0]["des"]})
+        }).catch(err =>{
+            alert(err)
+        })
+    }
+
     render() { 
         return (
             <div>
@@ -15,7 +28,7 @@ class About  extends Component {
                         <h1>Conference</h1>
                     </div>
                     <div className="info">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure similique neque minus, dolore, cumque ratione vitae aut reiciendis, nisi odit eius veritatis doloribus aperiam eveniet nemo aspernatur aliquid impedit quos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure labore nulla autem doloribus dolorum libero praesentium optio perferendis, incidunt reiciendis excepturi, sed omnis maxime quae repellat cupiditate odit, dolore amet. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam distinctio illo animi ullam omnis, est beatae commodi veritatis nihil, consequuntur rerum. Rerum laborum expedita sunt debitis? Aspernatur placeat quis laboriosam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae adipisci, dolore deserunt ea excepturi fugiat distinctio provident, ipsum non, quod alias officiis. Nisi culpa officiis, animi repellendus ex ullam facilis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem velit sunt ullam quia delectus cumque pariatur officia quis aut reprehenderit a natus culpa, ducimus inventore id rerum aliquid rem ad? <br/> <br/> Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis libero fugit voluptatibus cupiditate repudiandae eligendi excepturi odio neque, voluptatum ab cum deserunt ex ea impedit sit alias vel ipsum obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe molestiae nulla facere distinctio, est iusto impedit quod vitae nobis suscipit. Mollitia quod asperiores, ea distinctio maxime nesciunt! Possimus, a temporibus! </p>
+                        <p>{this.state.des}</p>
                     </div>
                 </div>
             </div>
