38ec2eaae3ca4797544a9a7138a77c2b8a383f52
nethsaraLiyanage <tharusharb1012@gmail.com>
2021-05-28 22:42:43 +0530

    work dhop das up 05

--- components/workshop_dashboard/AllWorkshops.jsx
+++ components/workshop_dashboard/AllWorkshops.jsx
@@ -2,7 +2,25 @@ import React, { Component } from 'react'
 import './../../assets/css/WorkshopDetails/WorkDashHome.css'
 
 class AllWorkshops extends Component {
+
+    constructor(props){
+        super(props)
+        this.state={
+            worksohps:[]
+        }
+    }
+
+    componentDidMount(){
+        this.fetchItems(); 
+    }
+
+    fetchItems(){
+        fetch("url").then(res => res.json()).then(data => this.setState({workshops:data})).catch(err => console.log(err))
+    }
+
+
     render() {
+        
         return (
             <div>
                 <div className="nt-dcard-cont">
@@ -23,26 +41,42 @@ class AllWorkshops extends Component {
                                 <tr>
                                     <th className="nt-td">Workshop Id</th>
                                     <th className="nt-td">Workshop Title</th>
+                                    <th className="nt-td">Workshop Date</th>
                                     <th className="nt-td">Conductor</th>
-                                    <th className="nt-td">Date</th>
+                                    <th className="nt-td">Created Date</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                {/* {this.state.workshops.map((workshop) => {
+                                    return(
+                                            <tr>
+                                                <td className="nt-td">{workshop._id}</td>
+                                                <td className="nt-td">{workshop.title}</td>
+                                                <td className="nt-td">{workshop.eventDate}</td>
+                                                <td className="nt-td">{workshop.conductor}</td>
+                                                <td className="nt-td">{workshop.dateCreated}</td>
+                                            </tr>
+                                    );
+                                })} */}
+
                                 <tr>
                                     <td className="nt-td">test id</td>
                                     <td className="nt-td">Test title</td>
+                                    <td className="nt-td">Test date</td>
                                     <td className="nt-td">Test conductor</td>
                                     <td className="nt-td">Test date</td>
                                 </tr>
                                 <tr>
                                     <td className="nt-td">test id</td>
                                     <td className="nt-td">Test title</td>
+                                    <td className="nt-td">Test date</td>
                                     <td className="nt-td">Test conductor</td>
                                     <td className="nt-td">Test date</td>
                                 </tr>
                                 <tr>
                                     <td className="nt-td">test id</td>
                                     <td className="nt-td">Test title</td>
+                                    <td className="nt-td">Test date</td>
                                     <td className="nt-td">Test conductor</td>
                                     <td className="nt-td">Test date</td>
                                 </tr>
--- components/workshop_dashboard/ApprovedCoordinators.jsx
+++ components/workshop_dashboard/ApprovedCoordinators.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import './../../assets/css/WorkshopDetails/WorkDashHome.css'
 
-class PendingCoordinators extends Component {
+class ApprovedCoordinators extends Component {
 
     constructor(props){
         super(props)
         this.state={
-            pendCrd:[]
+            approved:[]
         }
     }
 
@@ -15,7 +15,7 @@ class PendingCoordinators extends Component {
     }
 
     fetchItems(){
-        fetch("url").then(res => res.json()).then(data => this.setState({pendCrd:data})).catch(err => console.log(err))
+        fetch("url").then(res => res.json()).then(data => this.setState({approved:data})).catch(err => console.log(err))
     }
     
 
@@ -37,20 +37,16 @@ class PendingCoordinators extends Component {
                                     <th className="nt-td">Coordinator Id</th>
                                     <th className="nt-td">Coordinator Name</th>
                                     <th className="nt-td">Study Feild</th>
-                                    <th className="nt-td">Approve</th>
                                 </tr>
                             </thead>
                             <tbody>
 
-                            {this.state.pendCrd.map((pendCrd) => {
+                            {this.state.approved.map((approved) => {
                                 return(
                                         <tr>
-                                            <td className="nt-td">{pendCrd._id}</td>
-                                            <td className="nt-td">{pendCrd.name}</td>
-                                            <td className="nt-td">{pendCrd.studyFeild}</td>
-                                            <td className="nt-td">
-                                                <button className="nt-approve-btn"> Approve </button>
-                                            </td>
+                                            <td className="nt-td">{approved._id}</td>
+                                            <td className="nt-td">{approved.name}</td>
+                                            <td className="nt-td">{approved.studyFeild}</td>
                                         </tr>
                                 );
                             })}
@@ -58,25 +54,16 @@ class PendingCoordinators extends Component {
                                     <td className="nt-td">test id</td>
                                     <td className="nt-td">Test title</td>
                                     <td className="nt-td">Test conductor</td>
-                                    <td className="nt-td">
-                                        <button className="nt-approve-btn"> Approve </button>
-                                    </td>
                                 </tr>
                                 <tr>
                                     <td className="nt-td">test id</td>
                                     <td className="nt-td">Test title</td>
                                     <td className="nt-td">Test conductor</td>
-                                    <td className="nt-td">
-                                        <button className="nt-approve-btn"> Approve </button>
-                                    </td>
                                 </tr>
                                 <tr>
                                     <td className="nt-td">test id</td>
                                     <td className="nt-td">Test title</td>
                                     <td className="nt-td">Test conductor</td>
-                                    <td className="nt-td">
-                                        <button className="nt-approve-btn"> Approve </button>
-                                    </td>
                                 </tr>
                             </tbody>
                         </table>
@@ -89,4 +76,4 @@ class PendingCoordinators extends Component {
     }
 }
 
-export default PendingCoordinators
+export default ApprovedCoordinators
