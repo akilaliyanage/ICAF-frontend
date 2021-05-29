93827a9dbc342718bfa4c149ddc6d7af9c9c8a88
nethsaraLiyanage <tharusharb1012@gmail.com>
2021-05-29 21:20:26 +0530

    workshop dash 11

--- App.jsx
+++ App.jsx
@@ -33,7 +33,7 @@ export default class App extends React.Component {
                     <Route exact path="/admin">
                         <AdminDashboard />
                     </Route>
-                    <Route exact path="/workshop">
+                    <Route exact path="/Workshops">
                         <WorkshopTemplate />
                     </Route>
                     <Route exact path="/add-reviewer">
