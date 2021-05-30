80d507e6ee2b1e71d0b72e483791ddac245276e8
nethsaraLiyanage <tharusharb1012@gmail.com>
2021-05-30 08:02:00 +0530

    workshop dash 12

--- components/workshopDetails/WorkCordinatorReg.jsx
+++ components/workshopDetails/WorkCordinatorReg.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import ReactDOM from "react-dom";
 import './../../assets/css/WorkshopDetails/workReg.css'
 
+
+import config from '../../config.json'
+
 class WorkCordinatorReg extends Component {
 
     constructor(props){
@@ -12,17 +15,29 @@ class WorkCordinatorReg extends Component {
             email:"",
             password:"",
             confPassword:"",
-            regErros:""
+            regErros:"",
+            role: 'reg',
+            isAuth : false,
+            hiddenStatus: false,
+            logInURL: "/n-wc/workCon-login",
+            regURL:"/n-wc/workCon-register"
         }
 
+        this.role = this.role.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
-    handleSubmit(event){
-        console.log("Form submit")
-        event.preventDefault();
+
+    role(value){
+        if(value == 'reg'){
+            this.setState({role : 'reg', hiddenStatus : false})
+        }else if(value == 'login'){
+            this.setState({role : 'login', hiddenStatus : true})
+        }
     }
+
+    
 
     handleChange(event){
         this.setState({
@@ -30,31 +45,116 @@ class WorkCordinatorReg extends Component {
         });
     }
 
+    handleSubmit(){
+        
+        if(this.state.role == 'reg'){
+            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+            if(this.state.password != this.state.confPassword){
+                alert("Passwords mismatch!!!")
+
+            }else if(!re.test(String(this.state.email).toLowerCase())){
+                alert("Invalid E-mail address!")
+            }else{
+
+
+                const data = {
+                    name : this.state.name,
+                    studyField: this.state.studyField,
+                    email : this.state.email,
+                    password: this.state.password
+                }
+
+        
+                fetch(config.host + this.state.regURL,{
+                    method : 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body : JSON.stringify(data)
+                }).then(res => res.json()).then(data =>{
+
+                    alert("Registration Successfull!")
+                    
+                }).catch(err =>{
+                    console.log(err)
+                })
+            }
+        }
+
+        else if(this.state.role == 'login'){
+            const data = {
+                email : this.state.email,
+                password : this.state.password
+            }
+
+            fetch(config.host + this.state.logInURL,{
+                method : 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                  },
+                body : JSON.stringify(data)
+            }).then(res => res.json()).then(data =>{
+                console.log(data.token)
+    
+                if(data.token){
+                    window.localStorage.setItem('wc-token',data.token)
+                    window.localStorage.setItem('wc-id',data.id)
+                    window.localStorage.setItem('wc-email',data.email)
+
+                    alert("Login Successfull!")
+
+                    setInterval(() => {
+                        this.setState({isAuth : true});
+                    }, 2000);
+                }
+                
+            }).catch(err =>{
+                console.log(err)
+            })
+        }
+
+    }
+
 
     render() {
+
+        const back = {
+            background: '#00b5fddc',
+            color : 'white'
+        };
+
+        const back2 = {
+            background: 'none',
+            color : 'white'
+        };
+
         return (
             <div className="nt-r-signin" style={{ backgroundImage: `url(http://ig.com.my/wp-content/uploads/2017/11/profile-background.jpg)` }}>
                 <div className="nt-sign-card" style={{ backgroundImage: `url(http://ig.com.my/wp-content/uploads/2017/11/profile-background.jpg)` }}>
 
                     <div className="nt-signin">
                         <h1>I C A F</h1>
-                        <p className="nt-sign-head">Workshop Coordinator | SignUp</p>    
-                    </div>    
+                        <p className="nt-sign-head">Workshop Coordinator | {this.state.role == 'reg'?'SignUp' : 'LogIn'}</p>    
+                    </div>   
+
+                    <div className="nt-log-reg">
+                        <div onClick = {() => this.role('reg')} className="nt-rg-btns" style={this.state.role == 'reg' ? back : back2}> <b>SignUp</b> </div>
+                        <div onClick = {() => this.role('login')} className="nt-rg-btns" style={this.state.role == 'login' ? back : back2}> <b>LogIn</b> </div>
+                    </div> 
                 
-                    <form 
-                        onSubmit={this.handleSubmit}
-                    >
+                    <form>
                         <div className="nt-sign-form">
                             <div className="nt-sign-formitem">
                                 {/* <label className="nt-lable" htmlFor="">User Name</label><br /> */}
                                 <input type="text" name="name" className="nt-sign-input" placeholder="Your Name" 
-                                    value={this.state.name} onChange={this.handleChange} required
+                                    value={this.state.name} onChange={this.handleChange} required hidden={this.state.hiddenStatus}
                                 />
                             </div>
                             <div className="nt-sign-formitem">
                                 {/* <label className="nt-lable" htmlFor="">User Name</label><br /> */}
                                 <input type="text" name="studyField" className="nt-sign-input" placeholder="Your Feild of study" 
-                                    value={this.state.studyField} onChange={this.handleChange} required
+                                    value={this.state.studyField} onChange={this.handleChange} required hidden={this.state.hiddenStatus}
                                 />
                             </div>
                             <div className="nt-sign-formitem">
@@ -72,11 +172,11 @@ class WorkCordinatorReg extends Component {
                             <div className="nt-sign-formitem">
                                 {/* <label className="nt-lable" htmlFor="">User Name</label><br /> */}
                                 <input type="password" name="confPassword" className="nt-sign-input" placeholder="Confirm Password" 
-                                    value={this.state.confPassword} onChange={this.handleChange} required
+                                    value={this.state.confPassword} onChange={this.handleChange} required hidden={this.state.hiddenStatus}
                                 />
                             </div>
                             <div className="nt-formitem">
-                                <button className="nl-sign-form-btn" type="submit">SIGN UP</button>
+                                <button className="nl-sign-form-btn" type="submit"  onClick={this.handleSubmit}>{this.state.role == 'reg' ? 'Sign Up' : 'Log In'}</button>
                             </div>
 
                             {/* <p className="nt-warn">You won't be able to create workshops immediately. A reviewer should accept you as a workshop coordinator before.</p> */}
