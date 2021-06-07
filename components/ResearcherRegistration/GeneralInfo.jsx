a3772d95e26c2b5a596aa212b6ac6081bdfa8231
MahendraThammita <dhmmpthammita@gmail.com>
2021-06-07 20:28:55 +0530

    ResearcherRegistration UI changes added

--- components/ResearcherRegistration/GeneralInfo.jsx
+++ components/ResearcherRegistration/GeneralInfo.jsx
@@ -14,9 +14,9 @@ class RegFormGeneralInfo extends Component{
         return(
             <React.Fragment>
                 <div className='Regform-inner'>
-                    <p><b>Conference Dates : </b> Octomber 1, 2021 /  Octomber 2, 2021 / Octomber3, 2021</p>
-                    <p><b>Conference Location : </b> Main Audotoriam - SLIIT Malabe Campus, New Kandy Rd, Malabe</p>
-                    <p><b>Conference Time : </b> 8.00 to 17.00</p>
+                    <p className='text-light'><b className='text-light'>Conference Dates : </b> Octomber 1, 2021 /  Octomber 2, 2021 / Octomber3, 2021</p>
+                    <p className='text-light'><b className='text-light'>Conference Location : </b> Main Audotoriam - SLIIT Malabe Campus, New Kandy Rd, Malabe</p>
+                    <p className='text-light'><b className='text-light'>Conference Time : </b> 8.00 to 17.00</p>
                     <p style={{color:"green" , marginTop:20}} onclick={this.props.checkFn}>Please Fill-up the form below to register as a researcher of ICAF</p>
                 </div>
             </React.Fragment>
