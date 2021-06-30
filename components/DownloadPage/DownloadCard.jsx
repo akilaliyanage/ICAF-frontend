d6925dcd1b0044021bbc4327d5b64d7e664c137d
MahendraThammita <dhmmpthammita@gmail.com>
2021-06-30 18:24:56 +0530

    Common downloads page frontend compleated

--- components/DownloadPage/DownloadCard.jsx
+++ components/DownloadPage/DownloadCard.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 
 export default class DownloadCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+        }
+    }
     render() {
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+                <div>
+                    <div class="card col-3">
+                        <img src="../../assets/media/Mahen/PowerPoint-icon.png" class="card-img-top"/>
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Resource Name</h5>
+                            <a href="#!" class="btn btn-primary">Download</a>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
--- components/DownloadPage/DownloadsComponent.jsx
+++ components/DownloadPage/DownloadsComponent.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NavBar from './../landing_page/NavBar'
 import config from '../../config.json'
 import '../../assets/css/Downloads_Page/Downloads.css'
+import DownloadCard from './DownloadCard'
 
 export default class DownloadsComponent extends Component {
     constructor(props){
@@ -24,6 +25,19 @@ export default class DownloadsComponent extends Component {
             <React.Fragment>
                 <div className="common-download-nav-cover">
                     <NavBar items={this.state.navbar_items}/>
+                </div>
+
+                <div className='row m-5'>
+                    <div className='row d-flex justify-content-center'>
+                        <h1 className='text-center'>Research Paper Resources</h1>
+                        <DownloadCard/>
+                    </div>
+                </div>
+                <div className='row m-5'>
+                    <div className='row d-flex justify-content-center'>
+                        <h1 className='text-center'>Presentation Resources</h1>
+                        <DownloadCard/>
+                    </div>
                 </div>
             </React.Fragment>
         )
